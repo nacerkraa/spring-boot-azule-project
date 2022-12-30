4db30b08a30f6b76aa2e2d90b134f264555abe9c
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-12-30 15:06:14 +0100

    add eureka server

---
+++ eureka-server/src/main/java/com/kaido/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.kaido.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
